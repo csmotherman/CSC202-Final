87459c4477396f8dd4e01ed5eb421065d9147db1
Carter Smotherman <56828535+csmotherman@users.noreply.github.com>
2023-12-07 22:24:52 -0500

    Add files via upload

---
+++ Final.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(dplyr)
+
+data <- read.csv("/Users/cartersmotherman/Downloads/world_data_cleaned.csv")
+
+#Convert to numeric
+data$Year <- as.numeric(as.character(data$Year))
+
+#Find top 16 coutries in 2021
+top_16_countries <- head(arrange(subset(data, Year == 2021), desc(GDP)), 16)
+data_top_16 <- subset(data, Country %in% top_16_countries$Country)
+
+#Plotting
+ggplot(data_top_16, aes(x = Year, y = GDP, group = Country)) +
+  geom_line() +
+  facet_wrap(~ Country, nrow = 4, ncol = 4, scales = "free_y") +
+  labs(title = "GDP Trends of Top 16 Countries (in 2021) Over the Years", x = "Year", y = "GDP") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_x_continuous(breaks = seq(min(data_top_16$Year), max(data_top_16$Year), by = 5))
